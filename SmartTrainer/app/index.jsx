1a3eae82775cbc7f64e3173dc68e4b1155d99479
Coronilla <e.coronillahernandez@ugto.com>
2024-11-10 15:54:31 -0600

    Lista de ejercicios

--- SmartTrainer/app/index.jsx
+++ SmartTrainer/app/index.jsx
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import exercises from './../assets/data/exercises.json'
+import ExerciseListItem from './../components/exerciseListItem'
+
 
 export default function App() {
-    const exercise = exercises[0]
 
     return (
         <View style={styles.container}>
-            <View style = {styles.exerciseContainer}>
-            <Text style= {styles.exerciseName}> {exercise.name} </Text>
-            <Text style= {styles.exerciseSubtitle}>
-                 {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()} 
-            </Text>
-            </View>
-            <StatusBar style='auto'/>
+
+            <FlatList 
+                data={exercises}
+                contentContainerStyle={{ gap: 5 }}
+                keyExtractor={(item, index) => item.name + index}
+                renderItem={({ item }) => <ExerciseListItem item ={item}/> }
+            />
+            <StatusBar style="auto"/>
         </View>
     )
 }
@@ -23,21 +25,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'gainsboro',
         justifyContent: 'center',
-        padding: 10
-    },
-    exerciseContainer: {
-        backgroundColor: '#fff',
         padding: 10,
-        borderRadius: 10,
-        gap: 5
-
-    },
-    exerciseName: {
-        fontSize: 25,
-        fontWeight: 'bold'
-    },
-    exerciseSubtitle: {
-        color: 'dimgray'
-    },
-    
+        marginTop: 70,
+    }
 })
